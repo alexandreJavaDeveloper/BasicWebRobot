6c5030c1ff0be38e56860a27347eebb73605bf88
Alexandre <alexandreply@hotmail.com>
2017-04-04 23:29:45 -0300

    Just clean code.

--- src/main/java/com/openanswers/robot/Robot.java
+++ src/main/java/com/openanswers/robot/Robot.java
@@ -117,10 +117,7 @@ public class Robot
 			response.append(inputLine);
 		}
 
-		System.out.println("Response code: "+con.getResponseCode());
-
 		in.close();
-
 		System.out.println("Post response: " + response.toString());
 	}
 }
